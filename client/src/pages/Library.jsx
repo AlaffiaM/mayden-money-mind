b2df06d72ffdf917561f2e7bafcf04db18fb3e8a
Agbaje Stephen <agbajestephen5@gmail.com>
2026-08-10 23:32:48 +0100

    Update Library.jsx

--- client/src/pages/Library.jsx
+++ client/src/pages/Library.jsx
@@ -106,7 +106,7 @@ export default function Library() {
             </div>
             {playing === ep.id && (
               <div className="mt-4 pt-4 border-t border-gray-100">
-                <AudioPlayer src={ep.audioUrl} />
+                <AudioPlayer episodeId={ep.id} />
               </div>
             )}
           </div>
