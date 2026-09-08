15519e94a24dabec7b51259af77c8445b41f0abc
Agbaje Stephen <agbajestephen5@gmail.com>
2026-09-08 13:28:06 +0100

    Update subscriptions.js

--- server/src/routes/subscriptions.js
+++ server/src/routes/subscriptions.js
@@ -1,12 +1,8 @@
-
 import { Router } from "express";
 import { authenticate, requireVerified } from "../middleware/auth.js";
 import { getMine, getStatus, create, update, setAutoRenew } from "../controllers/subscriptionController.js";
 
 const router = Router();
-
-
-
 
 router.get("/mine", authenticate, requireVerified, getMine);
 router.get("/mine/status", authenticate, requireVerified, getStatus);
