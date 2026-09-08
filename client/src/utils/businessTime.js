0f4115ae77c56ecbca34aad761b7dc656d9fafc7
Agbaje Stephen <agbajestephen5@gmail.com>
2026-09-08 13:27:32 +0100

    Update businessTime.js

--- client/src/utils/businessTime.js
+++ client/src/utils/businessTime.js
@@ -1,15 +1,9 @@
-
-
-
-
-export const BUSINESS_UTC_OFFSET_MIN = 60; 
+export const BUSINESS_UTC_OFFSET_MIN = 60;
 const OFFSET_MS = BUSINESS_UTC_OFFSET_MIN * 60000;
-
 
 export function toBusinessDate(date) {
   return new Date(date.getTime() + OFFSET_MS);
 }
-
 
 export function businessDateStr(date) {
   const shifted = toBusinessDate(date);
@@ -19,11 +13,9 @@ export function businessDateStr(date) {
   return `${y}-${m}-${d}`;
 }
 
-
 export function businessDayOfWeek(date) {
   return toBusinessDate(date).getUTCDay();
 }
-
 
 export function businessToday() {
   const shifted = toBusinessDate(new Date());
