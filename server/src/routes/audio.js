77049dfe8d49761e0f08f73a446274e51364516f
Agbaje Stephen <agbajestephen5@gmail.com>
2026-09-08 13:06:25 +0100

    Update audio.js

--- server/src/routes/audio.js
+++ server/src/routes/audio.js
@@ -1,5 +1,5 @@
-// Protected audio streaming endpoint.
-// GET /api/audio?file=<path>&exp=<ts>&sig=<hmac>
+
+
 import { Router } from "express";
 import { stream } from "../controllers/audioController.js";
 
