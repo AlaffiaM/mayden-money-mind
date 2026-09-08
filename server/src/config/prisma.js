51dad80d9124a3de98d9e118bfded072c54a0f51
Agbaje Stephen <agbajestephen5@gmail.com>
2026-09-08 13:27:39 +0100

    Update prisma.js

--- server/src/config/prisma.js
+++ server/src/config/prisma.js
@@ -1,21 +1,13 @@
-
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const isProd = process.env.NODE_ENV === "production";
-
-
-
-
 
 const baseUrl = new URL(process.env.DATABASE_URL);
 const isRemote = !/localhost|127\.0\.0\.1|::1/i.test(baseUrl.hostname);
 baseUrl.searchParams.set("connection_limit", isProd ? "10" : "5");
 baseUrl.searchParams.set("connect_timeout", "8");
 baseUrl.searchParams.set("pool_timeout", "10");
-
-
-
 
 if (isRemote) {
   baseUrl.searchParams.set("sslmode", "no-verify");
