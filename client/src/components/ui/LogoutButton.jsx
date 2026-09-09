5823f50e2465744ce3cfd796bf7f903eb3d47690
Agbaje Stephen <agbajestephen5@gmail.com>
2026-09-09 23:59:40 +0100

    Update LogoutButton.jsx

--- client/src/components/ui/LogoutButton.jsx
+++ client/src/components/ui/LogoutButton.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useAuth } from "../../context/AuthContext";
+import { useAuth } from "../../context/useAuth";
 import { LogOut } from "lucide-react";
 
 export default function LogoutButton({ className, children, ariaLabel }) {
